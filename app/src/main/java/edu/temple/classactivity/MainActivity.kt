00719a166f85005c8ad5f5bd243bfbe0d07b058b
Yuto <63772196+seekrett@users.noreply.github.com>
2025-01-28 12:14:01 -0500

    'click me' button responds to user click

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,24 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // for single-use
+        // find view, not needing to declare variable && button clicked functionality
+        findViewById<Button>(R.id.button).setOnClickListener { view: View ->
+            (view as Button).text = "Button clicked"
+        }
     }
 }
